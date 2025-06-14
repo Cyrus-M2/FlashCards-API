5bb677ce1bd539efc72e9d7d82771bceb4e24755
Cyrus-M2 <devsben345@gmail.com>
2025-06-15 01:02:42 +0300

    imports

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
+//prisma client
+import { PrismaClient } from '@prisma/client';
 
 const app = express()
+//creating instance of prisma client
+const client = new PrismaClient()
 
 // testing the root
 app.get("/", (req, res) => {
     res.send("<h1>Welcome to our FlashCards API<h1/>")
 })
+
+//getting all our cards
+
+
+
 
 let port;
 if (process.env.PORT) {
@@ -13,9 +22,7 @@ if (process.env.PORT) {
 } else {
     port = 4000;
 }
-
 // const port = process.env.PORT || 4000
-
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 })
