86cbc1514cdcec6633e77e60239621c2eb921c40
Cyrus-M2 <devsben345@gmail.com>
2025-06-15 14:05:23 +0300

    added route for posting data to our flashcards

--- index.js
+++ index.js
@@ -6,14 +6,37 @@ const app = express()
 //creating instance of prisma client
 const client = new PrismaClient()
 
+app.use(express.json()) //middleware
+
 // testing the root
 app.get("/", (req, res) => {
     res.send("<h1>Welcome to our FlashCards API<h1/>")
 })
 
-//getting all our cards
+//getting all our flashcards
+app.get("/flashcards", async (req, res) => {
+    try {
+        const flashCards = await client.flashCard.findMany();
+        res.status(200).json(flashCards)
+    } catch (e) {
+        res.status(500).json({message: "Something went wrong"});
+    }
+});
 
-
+// route for adding data 
+app.post('/flashcards', (req, res) => {
+    try {
+        const {title, content} = req.body
+        client.flashCard.create({
+            data: {
+                title,
+                content
+            }
+        })
+    } catch (e) {
+        res.status(500).json({ message: "Something went wrong" })
+    }
+})
 
 
 let port;
